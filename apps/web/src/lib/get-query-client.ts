ee4208a19290b9c400fa0a3a2549595201a33f07
madflow <madflow@frikadelic.net>
2025-06-23 08:34:58 +0200

    Remove staleTime and fix indentation
    StaleTime was set to 1 minute, which might cause unnecessary refetching on the client. This change sets it to 0 for better performance. Also, fixed inconsistent indentation throughout the file.

--- apps/web/src/lib/get-query-client.ts
+++ apps/web/src/lib/get-query-client.ts
@@ -1,28 +1,26 @@
-import { QueryClient } from '@tanstack/react-query'
+import { QueryClient } from "@tanstack/react-query";
 
 const makeQueryClient = () => {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000, // 1 minute
+        staleTime: 0,
         refetchOnWindowFocus: false,
         retry: 1,
       },
     },
-  })
-}
+  });
+};
 
-let browserQueryClient: QueryClient | undefined = undefined
+let browserQueryClient: QueryClient | undefined = undefined;
 
 export const getQueryClient = () => {
-  if (typeof window === 'undefined') {
+  if (typeof window === "undefined") {
     // Server: always make a new query client
-    return makeQueryClient()
+    return makeQueryClient();
   } else {
     // Browser: make a new query client if we don't already have one
-    if (!browserQueryClient) browserQueryClient = makeQueryClient()
-    return browserQueryClient
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
   }
-}
+};
