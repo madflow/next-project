51de199f7f312c51979f55afea613b83eb773ef0
madflow <madflow@frikadelic.net>
2025-07-01 12:41:18 +0200

    move

--- packages/database/migrations/0005_normal_black_widow.sql
+++ packages/database/migrations/0005_normal_black_widow.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "datafiles" (
+CREATE TABLE "datasets" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" text NOT NULL,
 	"description" text,
@@ -8,10 +8,9 @@ CREATE TABLE "datafiles" (
 	"file_hash" text NOT NULL,
 	"s3_key" text NOT NULL,
 	"uploaded_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"project_id" uuid NOT NULL,
+	"organization_id" uuid NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"updated_at" timestamp with time zone,
-	"metadata" jsonb
+	"updated_at" timestamp with time zone
 );
 --> statement-breakpoint
-ALTER TABLE "datafiles" ADD CONSTRAINT "datafiles_project_id_projects_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."projects"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "datasets" ADD CONSTRAINT "datasets_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE cascade ON UPDATE no action;
