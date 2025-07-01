c073a077d286e56fa0c9984fb710adde222d1c21
madflow <madflow@frikadelic.net>
2025-07-01 18:41:05 +0200

    fast api template

---
+++ apps/analysis/analysis/db/models/__init__.py
@@ -0,0 +1,15 @@
+"""analysis models."""
+
+import pkgutil
+from pathlib import Path
+
+
+def load_all_models() -> None:
+    """Load all models from this folder."""
+    package_dir = Path(__file__).resolve().parent
+    modules = pkgutil.walk_packages(
+        path=[str(package_dir)],
+        prefix="analysis.db.models.",
+    )
+    for module in modules:
+        __import__(module.name)
