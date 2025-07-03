af776f61124fb4e3c5b627aa90dc36e5ab1fd8f1
madflow <madflow@frikadelic.net>
2025-07-03 14:29:27 +0200

    Renamed admin-datafiles.spec.ts to admin-datasets.spec.ts and updated test description accordingly.

--- packages/e2e-web/tests/admin-datasets.spec.ts
+++ packages/e2e-web/tests/admin-datasets.spec.ts
@@ -2,15 +2,15 @@ import { expect, test } from "@playwright/test";
 import { testUsers } from "../config";
 import { loginUser } from "../utils";
 
-test.describe("Admin Datafiles", () => {
-  test("should upload a datafile successfully", async ({ page }) => {
+test.describe("Admin Datasets", () => {
+  test("should upload a dataset successfully", async ({ page }) => {
     // Log in as admin
     await page.goto("/");
     await loginUser(page, testUsers.admin.email, testUsers.admin.password);
-    
+
     // Navigate to datafiles page
-    await page.goto("/admin/datafiles");
+    await page.goto("/admin/datasets");
     await page.waitForLoadState("networkidle");
-    await expect(page.getByTestId("admin.datafiles.page")).toBeVisible();
+    await expect(page.getByTestId("admin.datasets.page")).toBeVisible();
   });
 });
