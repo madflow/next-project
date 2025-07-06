458f1a6ea43730c817302ccc02a46477b9b8ee70
madflow <madflow@frikadelic.net>
2025-07-06 15:39:12 +0200

    Add dataset editor page

---
+++ apps/web/src/dal/dataset-variable.ts
@@ -0,0 +1,16 @@
+import "server-only";
+import { datasetVariable as entity, selectDatasetVariableSchema } from "@repo/database/schema";
+import { ListOptions, createList, withSessionCheck } from "@/lib/dal";
+
+const baseList = createList(entity, selectDatasetVariableSchema);
+
+async function listByDatasetFn(datasetId: string, options: ListOptions = {}) {
+  const listOptions: ListOptions = {
+    ...options,
+    filters: [...(options.filters || []), { column: "datasetId", operator: "eq", value: datasetId }],
+  };
+  return baseList(listOptions);
+}
+
+export const listByDataset = withSessionCheck(listByDatasetFn);
+
