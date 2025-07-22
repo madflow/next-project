3fa983995c4216a80866fb9a966dcaaa639629dc
madflow <madflow@frikadelic.net>
2025-07-22 15:22:41 +0200

    Merge translations

--- apps/web/src/global.ts
+++ apps/web/src/global.ts
@@ -1,24 +1,7 @@
 import "@tanstack/react-table";
 import { RowData } from "@tanstack/react-table";
 import { locales } from "@/i18n/config";
-import appAccount from "../messages/en/app.account.json";
-import appAdminSidebar from "../messages/en/app.admin-sidebar.json";
-import appAdminDashboard from "../messages/en/app.admin.dashboard.json";
-import adminDatasetEditor from "../messages/en/app.admin.dataset-editor.json";
-import appAdminDatasets from "../messages/en/app.admin.datasets.json";
-import appAdminOrganizations from "../messages/en/app.admin.organizations.json";
-import appAdminOrganizationsMembers from "../messages/en/app.admin.organizations.members.json";
-import appAdminProjects from "../messages/en/app.admin.projects.json";
-import appAdminUsers from "../messages/en/app.admin.users.json";
-import appAppSidebar from "../messages/en/app.app-sidebar.json";
-import appDatatable from "../messages/en/app.datatable.json";
-import appLocaleSwitcher from "../messages/en/app.locale-switcher.json";
-import appNavUser from "../messages/en/app.nav-user.json";
-import appProjectAdhoc from "../messages/en/app.project.adhoc.json";
-import authForgotPassword from "../messages/en/auth.forgot-password.json";
-import authLogin from "../messages/en/auth.login.json";
-import authResetPassword from "../messages/en/auth.reset-password.json";
-import authSignUp from "../messages/en/auth.sign-up.json";
+import messages from "../messages/en.json";
 
 declare module "@tanstack/react-table" {
   // eslint-disable-next-line
@@ -30,23 +13,6 @@ declare module "@tanstack/react-table" {
 declare module "next-intl" {
   interface AppConfig {
     Locale: (typeof locales)[number];
-    Messages: typeof appAccount &
-      typeof appAdminDashboard &
-      typeof adminDatasetEditor &
-      typeof appAdminSidebar &
-      typeof appAdminOrganizations &
-      typeof appAdminOrganizationsMembers &
-      typeof appAdminProjects &
-      typeof appAdminUsers &
-      typeof appAppSidebar &
-      typeof appDatatable &
-      typeof appLocaleSwitcher &
-      typeof appNavUser &
-      typeof authForgotPassword &
-      typeof authLogin &
-      typeof authResetPassword &
-      typeof authSignUp &
-      typeof appAdminDatasets &
-      typeof appProjectAdhoc;
+    Messages: typeof messages;
   }
 }
--- apps/web/src/i18n/request.ts
+++ apps/web/src/i18n/request.ts
@@ -4,26 +4,7 @@ import { getUserLocale } from "./locale";
 export default getRequestConfig(async () => {
   const locale = await getUserLocale();
 
-  const messages = {
-    ...(await import(`../../messages/${locale}/auth.login.json`)).default,
-    ...(await import(`../../messages/${locale}/auth.forgot-password.json`)).default,
-    ...(await import(`../../messages/${locale}/auth.sign-up.json`)).default,
-    ...(await import(`../../messages/${locale}/auth.reset-password.json`)).default,
-    ...(await import(`../../messages/${locale}/app.account.json`)).default,
-    ...(await import(`../../messages/${locale}/app.admin.organizations.json`)).default,
-    ...(await import(`../../messages/${locale}/app.admin.organizations.members.json`)).default,
-    ...(await import(`../../messages/${locale}/app.admin.users.json`)).default,
-    ...(await import(`../../messages/${locale}/app.datatable.json`)).default,
-    ...(await import(`../../messages/${locale}/app.admin.projects.json`)).default,
-    ...(await import(`../../messages/${locale}/app.nav-user.json`)).default,
-    ...(await import(`../../messages/${locale}/app.admin-sidebar.json`)).default,
-    ...(await import(`../../messages/${locale}/app.app-sidebar.json`)).default,
-    ...(await import(`../../messages/${locale}/app.locale-switcher.json`)).default,
-    ...(await import(`../../messages/${locale}/app.admin.dashboard.json`)).default,
-    ...(await import(`../../messages/${locale}/app.admin.datasets.json`)).default,
-    ...(await import(`../../messages/${locale}/app.admin.dataset-editor.json`)).default,
-    ...(await import(`../../messages/${locale}/app.project.adhoc.json`)).default,
-  };
+  const messages = (await import(`../../messages/${locale}.json`)).default;
   return {
     locale,
     messages,
