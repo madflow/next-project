2bf6dfa2033b090d8d2e6016f9368bd4257a3168
madflow <madflow@frikadelic.net>
2025-12-01 16:11:21 +0100

    Dynamic import

--- apps/web/src/components/project/adhoc-analysis-wrapper.tsx
+++ apps/web/src/components/project/adhoc-analysis-wrapper.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { ActiveThemeProvider } from "@/components/active-theme";
 import { useAdhocPersistence } from "@/hooks/use-adhoc-persistence";
 import { type Project } from "@/types/project";
-import { AdHocAnalysis } from "./adhoc-analysis";
+
+// Dynamically import AdHocAnalysis without SSR to avoid hydration issues with Radix UI Select
+const AdHocAnalysis = dynamic(() => import("./adhoc-analysis").then((mod) => ({ default: mod.AdHocAnalysis })), {
+  ssr: false,
+});
 
 type AdhocAnalysisWrapperProps = {
   project: Project;
