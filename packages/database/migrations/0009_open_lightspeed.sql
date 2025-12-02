c38d5ac22d06ed6dbe764e4ebdb8bca4cf011bc5
madflow <madflow@frikadelic.net>
2025-12-02 10:13:04 +0100

    Fix migrations

--- packages/database/migrations/0009_open_lightspeed.sql
+++ packages/database/migrations/0009_open_lightspeed.sql
@@ -1,1 +1,9 @@
-ALTER TABLE "invitations" ADD COLUMN "created_at" timestamp with time zone NOT NULL;
+ALTER TABLE "invitations" 
+ADD COLUMN "created_at" timestamp with time zone;
+
+UPDATE "invitations" 
+SET "created_at" = now()
+WHERE "created_at" IS NULL;
+
+ALTER TABLE "invitations" 
+ALTER COLUMN "created_at" SET NOT NULL;
