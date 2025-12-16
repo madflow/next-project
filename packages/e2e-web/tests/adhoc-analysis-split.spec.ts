7f62a62d8f608f74067f5323beb2a0909754cfd4
madflow <madflow@frikadelic.net>
2025-12-16 18:46:39 +0100

    Disable conditional test warnings for adhoc-analysis-split test
    - Add file-level eslint-disable for no-conditional-in-test rules
    - Test is exploratory/discovery-based and requires conditionals
    - All conditionals are intentional for robust test behavior
    - Test passes successfully (1 passed in 3.5s)
    - Zero lint warnings

--- packages/e2e-web/tests/adhoc-analysis-split.spec.ts
+++ packages/e2e-web/tests/adhoc-analysis-split.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable playwright/no-conditional-in-test, playwright/no-conditional-expect */
 import { Locator, expect, test } from "@playwright/test";
 import { testUsers } from "../config";
 import { loginUser } from "../utils";
