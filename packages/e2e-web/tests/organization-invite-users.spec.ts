65eafd2133d5cf9d142893adb390c1b8b9bd6e2f
madflow <madflow@frikadelic.net>
2026-03-17 08:44:20 +0100

    Harden e2e tests

--- packages/e2e-web/tests/organization-invite-users.spec.ts
+++ packages/e2e-web/tests/organization-invite-users.spec.ts
@@ -1,38 +1,8 @@
-import { Page, expect, test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { testUsers } from "../config";
-import { extractLinkFromMessage, getLatestEmail, loginUser, logoutUser } from "../utils";
-import { smtpServerApi } from "../utils";
+import { inviteUser, loginUser, logoutUser, selectOrganization, visitAcceptPageFromEmail } from "../utils";
 
 const orgWithPermission = "Test Organization 3";
-
-async function inviteUser(page: Page, userEmail: string) {
-  // /api/auth/organization/get-full-organization
-  const getOrgResponsePromise = page.waitForResponse("api/auth/organization/get-full-organization");
-  await page.getByTestId("app.organization-switcher").click();
-  await getOrgResponsePromise;
-  await page.getByTestId("app.organization-switcher.invite").click();
-  await page.getByTestId("admin.users.invite.form.email").fill(userEmail);
-  const inviteResponsePromise = page.waitForResponse("api/auth/organization/invite-member");
-  await page.getByTestId("admin.users.invite.form.submit").click();
-  await inviteResponsePromise;
-  await page.getByTestId("invite-user-modal.close").click();
-}
-
-async function selectOrganization(page: Page, orgName: string) {
-  const organizationSwitcher = page.getByTestId("app.organization-switcher");
-  await organizationSwitcher.click();
-  await page.getByText(orgName, { exact: true }).click();
-  await expect(organizationSwitcher.locator("span")).toHaveText(orgName);
-}
-
-async function visitAcceptPageFromEmail(page: Page, userEmail: string) {
-  const message = await getLatestEmail(userEmail);
-  const acceptLink = await extractLinkFromMessage(message, "accept-invitation");
-  expect(acceptLink).toBeTruthy();
-  await smtpServerApi.deleteMessagesBySearch({ query: `to:"${userEmail}"` });
-  await page.goto(acceptLink);
-}
-
 
 test.describe("User invitations", () => {
   test("an owner can invite an existing user", async ({ page }) => {
