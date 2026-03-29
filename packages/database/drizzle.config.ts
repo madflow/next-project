6e93444f3f9e621d5e2c0220c50c481346a2c6d4
madflow <madflow@frikadelic.net>
2026-03-29 14:34:17 +0200

    DB Sentry

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import "./instrumentation.ts";
 
 export default defineConfig({
   out: "./migrations",
   schema: "./dist/schema/index.mjs",
   dialect: "postgresql",
+  verbose: true,
   dbCredentials: {
     url: process.env.DATABASE_URL!,
     ssl: { rejectUnauthorized: false },
