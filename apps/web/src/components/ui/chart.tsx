55d150be6632fcd141a810ab399548e4a9ce76d9
madflow <madflow@frikadelic.net>
2026-05-04 14:57:36 +0200

    Fixup

--- apps/web/src/components/ui/chart.tsx
+++ apps/web/src/components/ui/chart.tsx
@@ -61,6 +61,13 @@ function ChartContainer({
 }) {
   const uniqueId = React.useId()
   const chartId = `chart-${id ?? uniqueId.replace(/:/g, "")}`
+  const chartVariables = chartColors
+    ? (Object.fromEntries(Object.entries(chartColors).map(([key, value]) => [`--${key}`, value])) as React.CSSProperties)
+    : undefined
+  const mergedStyle = {
+    ...chartVariables,
+    ...props.style,
+  } satisfies React.CSSProperties
 
   return (
     <ChartContext.Provider value={{ config }}>
@@ -71,13 +78,7 @@ function ChartContainer({
           "flex aspect-video justify-center text-xs [&_.recharts-cartesian-axis-tick_text]:fill-muted-foreground [&_.recharts-cartesian-grid_line[stroke='#ccc']]:stroke-border/50 [&_.recharts-curve.recharts-tooltip-cursor]:stroke-border [&_.recharts-dot[stroke='#fff']]:stroke-transparent [&_.recharts-layer]:outline-hidden [&_.recharts-polar-grid_[stroke='#ccc']]:stroke-border [&_.recharts-radial-bar-background-sector]:fill-muted [&_.recharts-rectangle.recharts-tooltip-cursor]:fill-muted [&_.recharts-reference-line_[stroke='#ccc']]:stroke-border [&_.recharts-sector]:outline-hidden [&_.recharts-sector[stroke='#fff']]:stroke-transparent [&_.recharts-surface]:outline-hidden",
           className
         )}
-        style={
-          chartColors
-            ? Object.fromEntries(
-                Object.entries(chartColors).map(([key, value]) => [`--${key}`, value])
-              ) as React.CSSProperties
-            : undefined
-        }
+        style={mergedStyle}
         {...props}
       >
         <ChartStyle id={chartId} config={config} />
