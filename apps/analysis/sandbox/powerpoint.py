d9040cd9e49d30ac1d578738cc913229d220986a
madflow <madflow@frikadelic.net>
2026-06-02 14:43:27 +0200

    Fix powerpoint export

--- apps/analysis/sandbox/powerpoint.py
+++ apps/analysis/sandbox/powerpoint.py
@@ -2,7 +2,7 @@
 
 from pptx import Presentation
 from pptx.chart.data import CategoryChartData
-from pptx.enum.chart import XL_CHART_TYPE
+from pptx.enum.chart import XL_CHART_TYPE, XL_LABEL_POSITION
 from pptx.util import Inches
 
 
@@ -19,7 +19,15 @@ def main() -> None:
 
     # add chart to slide --------------------
     x, y, cx, cy = Inches(2), Inches(2), Inches(6), Inches(4.5)
-    slide.shapes.add_chart(XL_CHART_TYPE.COLUMN_CLUSTERED, x, y, cx, cy, chart_data)
+    chart = slide.shapes.add_chart(XL_CHART_TYPE.BAR_CLUSTERED, x, y, cx, cy, chart_data).chart
+
+    plot = chart.plots[0]
+    plot.has_data_labels = True
+    data_labels = plot.data_labels
+    data_labels.position = XL_LABEL_POSITION.RIGHT
+
+    category_axis = chart.category_axis
+    category_axis.reverse_order = True
 
     prs.save("charts.pptx")
 
